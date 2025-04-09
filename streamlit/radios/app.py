a1c5e78f2d58d4d47fd25783629bd6025a92ed3c
Your Name <you@example.com>
2025-04-09 23:40:42 +0800

    add radios

---
+++ streamlit/radios/app.py
@@ -0,0 +1,67 @@
+import streamlit as st
+from dashscope import ImageSynthesis, VideoSynthesis
+from util import req_synthesis
+
+default_prompt = "一只狗在海边溜达"
+default_url = "https://static.streamlit.io/examples/dog.jpg"
+null_url = "https://via.placeholder.com/400x300.png?text=结果尚未生成"
+
+
+# 生成图像请求
+def call_req(prompt, img_url):
+    if st.session_state.selected_feature == "img2video":
+        urls, tm, err = req_synthesis(
+            call_func=VideoSynthesis.call,
+            model=VideoSynthesis.Models.wanx_2_1_t2v_turbo,
+            prompt=prompt,
+            img_url=img_url,
+            is_video=True,
+        )
+    else:
+        urls, tm, err = req_synthesis(
+            call_func=ImageSynthesis.call,
+            model=ImageSynthesis.Models.wanx_v1,
+            prompt=prompt,
+            img_url=img_url,
+            style="<3d cartoon>",
+            is_video=False,
+        )
+    if err:
+        st.error(err)
+        return None
+
+    if not urls:
+        return
+    if is_video:
+        st.session_state.generated_videos = urls
+    else:
+        st.session_state.generated_imgs = urls
+
+
+feature = st.radio("功能", options=["img2img", "img2video"])
+st.session_state.selected_feature = feature
+st.write(f"你选择了 {feature} 功能")
+prompt = st.text_input("prompt", placeholder=default_prompt)
+img_url = st.text_input("img url", placeholder=default_url) or default_url
+col1, col2 = st.columns([1, 1])
+with col1:
+    st.text("原始图片")
+    st.image(img_url, width=400)
+with col2:
+    st.text("生成结果")
+    is_video = st.session_state.selected_feature == "img2video"
+    result_key = "generated_videos" if is_video else "generated_imgs"
+    urls = st.session_state.get(result_key, "")
+    urls = [null_url] if not is_video and not urls else urls
+    if is_video:
+        if len(urls) > 0:
+            st.video(urls[0])
+    else:
+        st.image(urls, width=400)
+
+st.button(
+    "提交",
+    on_click=call_req,
+    args=[prompt, img_url],
+    use_container_width=True,
+)
---
+++ streamlit/radios/util.py
@@ -0,0 +1,41 @@
+from http import HTTPStatus
+from datetime import datetime
+
+
+def do_request(call_func, call_args, result_handler):
+    start_time = datetime.now()
+    rsp = call_func(**call_args)
+    end_time = datetime.now()
+    elapsed_time = str(end_time - start_time)
+    print(rsp)
+    if rsp.status_code != HTTPStatus.OK:
+        print(
+            f"请求失败, status_code: {rsp.status_code}, code: {rsp.code}, message: {rsp.message}"
+        )
+        return (
+            "请求失败，请重试。",
+            None,
+            elapsed_time,
+            f"Error: {rsp.status_code}, {rsp.code}, {rsp.message}",
+        )
+    if rsp.output.task_status != "SUCCEEDED":
+        return "", elapsed_time, rsp.output.message
+    return result_handler(rsp.output), elapsed_time, None
+
+
+def req_synthesis(call_func, model, prompt, img_url, style=None, n=1, is_video=False):
+    args = {"model": model, "prompt": prompt, "img_url": img_url, "n": n}
+    if style:
+        args["style"] = style
+    if not is_video:
+        args["ref_img"] = img_url
+
+    result_handler = lambda output: (
+        [output.video_url]
+        if isinstance(output, object) and is_video
+        else [r.url for r in output.results] if isinstance(output, object) else []
+    )
+
+    return do_request(
+        call_func=call_func, call_args=args, result_handler=result_handler
+    )
