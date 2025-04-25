bcee5451dc78744386e7844c6624ce723ac041b2
kunpeng <kunpeng@gmail.com>
2025-04-25 15:50:32 +0800

    add translate ui

---
+++ apps/translate/app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from tr import translate
+
+def do_translate(text, input_language, output_language):
+    st.session_state.translation = translate(text, input_language, output_language)
+
+cols = st.columns(2)
+with cols[0]:
+    input_language = st.selectbox("输入语言", ["chinese", "english"])
+    original_text = st.text_area(
+        "原文",
+        key="original_text",
+        placeholder="请输入要翻译的内容",
+        height=800,
+    )
+with cols[1]:
+    output_language = st.selectbox("输出语言", ["chinese", "english"])
+    st.text_area(
+        "译文",
+        key="translation",
+        placeholder="请输入要翻译的内容",
+        height=800,
+    )
+st.button(
+    "翻译",
+    on_click=do_translate,
+    args=[original_text, input_language, output_language],
+    use_container_width=True,
+)
