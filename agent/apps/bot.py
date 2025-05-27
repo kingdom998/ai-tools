28cb6de45fdd257ae5606cbe266e986b8b5de824
kunpeng <kunpeng@gmail.com>
2025-05-27 15:58:23 +0800

    rn

---
+++ agent/apps/bot.py
@@ -0,0 +1,64 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from util._llm import llm
+from langchain_core.messages import HumanMessage, BaseMessage
+from langgraph.graph.message import add_messages
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import START, StateGraph
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from typing import Sequence
+from typing_extensions import Annotated, TypedDict
+
+
+class State(TypedDict):
+    messages: Annotated[Sequence[BaseMessage], add_messages]
+    language: str
+
+
+prompt_template = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a helpful assistant. Answer all questions to the best of your ability in {language}.",
+        ),
+        MessagesPlaceholder(variable_name="messages"),
+    ]
+)
+
+
+def call_model(state: State):
+    prompt = prompt_template.invoke(state)
+    response = llm.invoke(prompt)
+    return {"messages": [response]}
+
+
+memory = MemorySaver()
+workflow = StateGraph(state_schema=State)
+workflow.add_edge(START, "model")
+workflow.add_node("model", call_model)
+app = workflow.compile(checkpointer=memory)
+
+
+def one():
+    config = {"configurable": {"thread_id": "abc123"}}
+
+    query = "Hi! I'm Bob."
+    language = "Chinese"
+
+    input_messages = [HumanMessage(query)]
+    output = app.invoke({"messages": input_messages, "language": language}, config)
+    output["messages"][-1].pretty_print()
+
+    query = "What is my name?"
+
+    input_messages = [HumanMessage(query)]
+    output = app.invoke(
+        {"messages": input_messages},
+        config,
+    )
+    output["messages"][-1].pretty_print()
+
+if __name__ == "__main__":
+    one()
--- agent/util/_llm.py
+++ agent/util/_llm.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from langchain_openai import AzureChatOpenAI
 from langchain_core.tools import tool
 
+
 @tool
 def add(a: int, b: int) -> int:
     """Adds a and b."""
@@ -51,51 +52,10 @@ llm = AzureChatOpenAI(
 )
 
 
-@tool
-def add(a: int, b: int) -> int:
-    """Adds a and b."""
-    print(f"add({a}, {b})")
-
-    return a + b
-
-
-@tool
-def multiply(a: int, b: int) -> int:
-    """Multiplies a and b."""
-    return a * b
-
-
-@tool
-def img_resize(w: int, h: int) -> int:
-    """Resizes an image to the given width and height."""
-    print(f"img_resize({w}, {h})")
-    return w * h
-
-
-@tool
-def img2img(w: int, h: int) -> int:
-    """generates an image from a given width and height."""
-    print(f"img2img({w}, {h})")
-    return w * h
-
-
-def route(result):
-    if isinstance(result, AgentFinish):
-        return result.return_values["output"]
-    else:
-        tools = {
-            "add": add,
-            "multiply": multiply,
-        }
-        return tools[result.tool].run(result.tool_input)
-
-
 if __name__ == "__main__":
     from langchain_core.prompts import ChatPromptTemplate
 
-    prompt = ChatPromptTemplate.from_messages(
-        [("system", "You are a helpful assistant"), ("user", "{input}")]
-    )
+    prompt = ChatPromptTemplate.from_messages([("system", "You are a helpful assistant"), ("user", "{input}")])
 
     tools = [add, multiply, img_resize]
     llm_with_tools = llm.bind_tools(tools)
